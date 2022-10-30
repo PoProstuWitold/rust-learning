85fc0d63dd39d21e1674e2b5eee96286a19af03c
PoProstuWitold <witekzawada@o2.pl>
2022-10-30 11:13:52 +0100

    ch 12 - minigrep

---
+++ exercises/2-rust-book/ch-12/minigrep/src/main.rs
@@ -0,0 +1,19 @@
+use std::{ process, env::{ args } };
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Problem reading file: {e}");
+        process::exit(1);
+    }
+}
